d30a3abeb7de7f786a58dda8296daa9bb1fbe638
igare4eg <sokolik1063@mail.ru>
2025-06-20 17:28:25 -0300

    UPD 5 20.06

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QApplication, QInputDialog, QMessageBox
 import sys
 from ui import DiaryApp
 from db import init_db
 
+
+def check_password():
+    correct = "pushki"
+    password, ok = QInputDialog.getText(None, "Пароль", "Введите пароль:")
+    if not ok or password != correct:
+        QMessageBox.critical(None, "Ошибка", "Неверный пароль!")
+        sys.exit()
+
+
 if __name__ == "__main__":
     init_db()
     app = QApplication(sys.argv)
+    check_password()
     window = DiaryApp()
     window.show()
     sys.exit(app.exec_())
